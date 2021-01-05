985ed14d069685beabc4f0a1357f391f6db59117
guyi <guyi@360pai.com>
2021-01-05 14:51:59 +0800

    12345

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from django.shortcuts import render
+#coding: utf-8
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import  auth
 import logging
+from apps.accounts.forms import Login
 
 @login_required
 def index(request):
@@ -12,5 +14,19 @@ def index(request):
 
 
 def test(request):
-    print(auth.get_user(request),auth._get_user_session_key(request))
-    return render(request, 'test.html')
+    if request.method == "GET":
+        forms = Login()
+        return render(request, 'test.html', {'forms': forms})
+    if request.method == "POST":
+        forms = Login(request.POST)
+        if forms.is_valid():
+            username=forms.cleaned_data['username']
+            password=forms.cleaned_data['password']
+            user = auth.authenticate(username=username, password=password)
+            print("user:%s" % user)
+            if user:
+                auth.login(request,user)
+                return redirect('/')
+            else:
+                msg = "登陆失败"
+                return render(request, 'test.html', locals())
