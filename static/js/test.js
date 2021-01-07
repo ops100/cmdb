3a6e3b0b211e467e79c907628f290526ec1e1ea7
guyi <guyi@360pai.com>
2021-01-07 16:10:53 +0800

    12345

--- static/js/test.js
+++ static/js/test.js
@@ -1,34 +1,44 @@
-window.onload = function () {
-    var winw = window.innerWidth;
-    var menuw = document.getElementById("menu").offsetWidth;
-    var topmenuw = winw-menuw;
-    var s = " winw: " + winw + " menuw: " + menuw + " topmenuw: "+  topmenuw
-    console.log(s)
-    document.getElementById("topmenu").style.width=topmenuw + "px";
-    document.getElementById("topmenu").innerText=s;
 
-}
-//
-// window.onresize = function () {
-//     var winw = window.innerWidth;
-//     var menuw = document.getElementById("menu").offsetWidth;
-//     var topmenuw = winw-menuw;
-//     var contentw = topmenuw;
-//     var s = "winw: " + winw + "menuw: " + menuw + "topmenuw: "+  topmenuw
-//     console.log(s)
-//     document.getElementById("topmenu").style.width=topmenuw + "px";
-//     document.getElementById("topmenu").innerText=s;
-//     document.getElementById("content").innerText=s;
-//
-// }
 
-document.getElementById("menu").onresize = function () {
-    var winw = window.innerWidth;
-    var menuw = document.getElementById("menu").offsetWidth;
-    var topmenuw = winw-menuw;
-    var s = "winw: " + winw + "menuw: " + menuw + "topmenuw: "+  topmenuw
-    console.log(s)
-    document.getElementById("topmenu").style.width=topmenuw + "px";
-    document.getElementById("topmenu").innerText=s;
 
-}
+
+    	$(function(){
+    		/*判断上次是否勾选记住密码和自动登录*/
+    		var check1s=localStorage.getItem("check1");
+    		var check2s=localStorage.getItem("check2");
+    		var oldName=localStorage.getItem("userName");
+    		var oldPass=localStorage.getItem("passWord");
+    		if(check1s=="true"){
+    			 $("#login-username").val(oldName);
+    			 $("#login-password").val(oldPass);
+    			 $("#check1").prop('checked',true);
+    		}else{
+    			 $("#login-username").val('');
+    			 $("#login-password").val('');
+    			 $("#check1").prop('checked',false);
+    		}
+    		if(check2s=="true"){
+    			$("#check2").prop('checked',true);
+    			$("#loginFrom").submit();
+    			//location="https://www.baidu.com?userName="+oldName+"&passWord="+oldPass;//添加退出当前账号功能
+    		}else{
+    			$("#check2").prop('checked',false);
+    		}
+    		/*拿到刚刚注册的账号*/
+    		/*if(localStorage.getItem("name")!=null){
+    			$("#login-username").val(localStorage.getItem("name"));
+    		}*/
+    		/*登录*/
+    		$("#login").click(function(){
+    			var userName=$("#login-username").val();
+    			var passWord=$("#login-password").val();
+    			/*获取当前输入的账号密码*/
+    			localStorage.setItem("userName",userName)
+    			localStorage.setItem("passWord",passWord)
+    			/*获取记住密码  自动登录的 checkbox的值*/
+    			var check1 = $("#check1").prop('checked');
+	    		var check2 = $('#check2').prop('checked');
+	    		localStorage.setItem("check1",check1);
+	    		localStorage.setItem("check2",check2);
+    		})
+    	})
