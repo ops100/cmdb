693aecb86901eef7c08fd9e5bbe2126af7e31373
guyi <guyi@360pai.com>
2021-01-15 23:16:31 +0800

    更新

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
 
     // 左侧菜单
     $("u").click(function () {
-        $(this).next('ul').css( "box-shadow","1px 1px 1px #aaaaaa");
+        // $(this).toggleClass({'text-shadow': '0px 5px 5px' });
+        // $(this).next('ul').css( "box-shadow","1px 1px 1px #aaaaaa");
         $(this).next('ul').toggle(20);
     }
     );
